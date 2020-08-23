e207da3bac6f86c471d2bc7fa127be17592de3ce
FK-satoakira <69459763+FK-satoakira@users.noreply.github.com>
2020-08-23 17:05:09 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,50 @@
+package list04_03;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Hero {
+	public String name; /*}*/  /* equals()をオーバーライドしていない */
+
+	//	ここは自分で記述	/*①	*/
+public boolean equals(Object o) {
+	if(o==this)return true;
+	if(o==null)return false;
+	if(!(o instanceof Hero))return false;
+	Hero h = (Hero)o;
+	if(!this.name.trim().equals(h.name.trim())) {return false;}
+	return true;
+}
+
+//@Override
+//public String toString() {
+//	return "Hero [name=" + name + "]";
+//	}
+}
+/*---------*/
+
+public class Main {
+    public static void main(String[] args) {
+//        Hero型のリストを作る
+        List<Hero> list = new ArrayList<Hero>();
+        Hero h1 = new Hero();h1.name="test";
+//        Hero h2 = new Hero();h1.name="test";
+        list.add(h1);
+        list.add(h1);
+        list.add(h1);
+        list.add(h1);
+        System.out.println("要素数=" + list.size());
+        h1 = new Hero();
+        h1.name="test";
+        list.add(h1);
+        System.out.println("要素数=" + list.size());
+        int hoken = 0;
+        while(list.size()>0 || hoken==10) {
+        	System.out.println(list.remove(h1));
+            System.out.println("要素数=" + list.size());
+            hoken++;
+        }
+    }
+}
+//これだと上手くいくが、h2、h3、など新インスタンスを作り、addすると失敗するのはなぜ？
